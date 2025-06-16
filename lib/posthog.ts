59fd5e245ea41e4ccb984ba49b818091d053d7d8
Hypnos <47209391+Jace254@users.noreply.github.com>
2025-06-16 18:02:26 +0300

    chore: add posthog

---
+++ lib/posthog.ts
@@ -0,0 +1,13 @@
+import { PostHog } from "posthog-node"
+
+export default function PostHogClient() {
+  const posthogClient = new PostHog(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    // @ts-expect-error
+    capture_pageview: 'history_change',
+    flushAt: 1,
+    flushInterval: 0,
+  })
+
+  return posthogClient
+}
